4ccb1600a6a71445e4217cb98decfda40096a50c
Oscar Holmér <oscar_holmer@msn.com>
2019-11-18 21:20:05 +0100

    Create AutoBranchName.meta.js

---
+++ Jira/AutoBranchName.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Jira auto branchname
+// @namespace    https://gitea.weep.se/weep/TamperScripts/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Weep
+// @updateURL    https://github.com/weep/TamperScripts/raw/master/Jira/AutoBranchName.meta.js
+// @downloadURL  https://github.com/weep/TamperScripts/raw/master/Jira/AutoBranchName.user.js
+// @include      /^https?://jira\..*\.com/.*$/
+// @grant        none
+// ==/UserScript==
